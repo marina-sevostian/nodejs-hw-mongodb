e0a907b9ebe144c4f3aa507c6c1beb1806566040
Marina.S. <marina.sevostian@gmail.com>
2025-03-28 08:44:22 +0200

    add task 3 4 5 6

---
+++ src/services/contacts.js
@@ -0,0 +1,11 @@
+import ContactsCollection from '../db/models/Contact.js';
+
+export async function getAllContacts() {
+  const contacts = await ContactsCollection.find();
+  return contacts;
+}
+
+export async function getContactById(contactId) {
+  const contact = await ContactsCollection.findById(contactId);
+  return contact;
+}
