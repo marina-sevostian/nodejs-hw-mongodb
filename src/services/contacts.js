55de6f7c2d68a6730817482830657ab0727b8f04
Marina.S. <marina.sevostian@gmail.com>
2025-04-03 18:08:54 +0300

    add filter params and fixed sort params

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -7,11 +7,21 @@ export const getAllContacts = async ({
   perPage,
   sortOrder = SORT_ORDER.ASC,
   sortBy = '_id',
+  filter = {},
 }) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
   const contactsQuery = ContactsCollection.find();
+
+  if (filter.type) {
+    contactsQuery.where('contactType').equals(filter.type);
+  }
+
+  if (filter.isFavourite !== undefined) {
+    contactsQuery.where('isFavourite').equals(filter.isFavourite);
+  }
+
   const contactsCount = await ContactsCollection.find()
     .merge(contactsQuery)
     .countDocuments();
