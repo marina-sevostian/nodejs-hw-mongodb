62493fb885530880f5d4018472e4848a1e69a6ea
Marina.S. <marina.sevostian@gmail.com>
2025-04-09 19:14:04 +0300

    update contact

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -77,8 +77,7 @@ export const updateContact = async (
   // if (!rawResult || !rawResult.value) return null;
 
   return {
-    contact: rawResult,
-    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+    rawResult,
   };
 };
 
